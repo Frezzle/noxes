8c93ff399a1f85fc66bbab7b9b6260230f5fc73b
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2019-01-19 21:38:19 +0000

    Basic API with root endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", homePage)
+
+	address := "localhost:9876"
+	log.Printf("Listening on %s", address)
+	log.Fatal(http.ListenAndServe(address, nil))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi!")
 }
